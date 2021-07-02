bf1691b56bdc2a943d19be9f95c0e849efdc3098
shagun25 <goyalshagun25@gmail.com>
2021-07-02 13:42:03 +0530

    assignment 3

--- Node.js the Complete Guide/app.js
+++ Node.js the Complete Guide/app.js
@@ -1,13 +1,20 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/user',(req,res,next)=>{
-    res.send('<h1>This is user midware</h1>');
- });
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/',(req,res,next)=>{
-    res.send('<h1>This is / midware</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+})
 app.listen(3000);
