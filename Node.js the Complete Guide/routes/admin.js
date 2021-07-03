260ded67cd1f9b3eb9a7557c24ed590ed87b6dd6
shagun25 <goyalshagun25@gmail.com>
2021-07-03 22:15:01 +0530

    sharing data across requests and users

--- Node.js the Complete Guide/routes/admin.js
+++ Node.js the Complete Guide/routes/admin.js
@@ -6,12 +6,15 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.use('/add-product',(req,res,next)=>{
+const products = [];
+
+router.get('/add-product',(req,res,next)=>{
     res.sendFile(path.join(rootDir,'views','add-product.html'));
   });
 router.post('/add-product',(req,res,next)=>{
-     console.log(req.body);
+     products.push({title: req.body.title});
      res.redirect('/');
   });
 
-module.exports = router;
+exports.router = router;
+exports.products = products;
--- Node.js the Complete Guide/routes/shop.js
+++ Node.js the Complete Guide/routes/shop.js
@@ -4,9 +4,13 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
+const admindata = require('./admin.js'); 
+
 const router = express.Router();
 
+
 router.get('/',(req,res,next)=>{
+    console.log(admindata.products);
     res.sendFile(path.join(rootDir,'views','shop.html'));
 });
 
